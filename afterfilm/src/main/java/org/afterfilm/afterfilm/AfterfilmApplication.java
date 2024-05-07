ca4d6fe06be230d19b5e99312b62475a0a37d622
bohodays <bohodays0316@gmail.com>
2024-05-07 20:20:27 +0900

    init

---
+++ afterfilm/src/main/java/org/afterfilm/afterfilm/AfterfilmApplication.java
@@ -0,0 +1,13 @@
+package org.afterfilm.afterfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterfilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterfilmApplication.class, args);
+	}
+
+}
---
+++ afterfilm/src/test/java/org/afterfilm/afterfilm/AfterfilmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.afterfilm.afterfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterfilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
