483fd5b2f702b52b9866d5cc97874dd920cf24f5
bohodays <bohodays0316@gmail.com>
2024-06-07 02:57:45 +0900

    feat : boxOffice api 개발

---
+++ afterfilm/src/main/java/org/afterfilm/afterfilm/domain/BoxOffice.java
@@ -0,0 +1,38 @@
+package org.afterfilm.afterfilm.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.afterfilm.afterfilm.dto.BoxOfficeDto;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoxOffice {
+
+    private String movieId;
+    private String title;
+    private String plot;
+    private int rank;
+    private String rlsDate;
+    private int runtime;
+    private List<String> genres;
+    private List<String> posterUrls;
+
+    public BoxOfficeDto toDto() {
+        return BoxOfficeDto.builder()
+                .movieId(movieId)
+                .title(title)
+                .plot(plot)
+                .rank(rank)
+                .rlsDate(rlsDate)
+                .runtime(runtime)
+                .genres(genres)
+                .posterUrls(posterUrls)
+                .build();
+    }
+}
